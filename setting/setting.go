829e44387b08dd3ee8dcc28b0f1e4f7186ebc701
Ahri <ahri.public@gmail.com>
2024-02-21 13:25:53 +0900

    4.1  Direct communication with the agent is possible even without a scheduler.

--- setting/setting.go
+++ setting/setting.go
@@ -1,7 +1,12 @@
 package setting
 
 /* ----- Server Setting ----- */
-const ServerPort string = "2000" // Edit this
+const ServerPort string = "6000" // Edit this
 
 /* ----- Scheduler Server Setting ----- */
+// If you are not using a scheduler, change the 'SchedulerActive' variable to false.
+const SchedulerActive bool = false           // Edit this
 const SchedulerUrl string = "localhost:8000" // Edit this
+
+// If you are not using a Scheduler, please set the AgentURL.
+const AgentURL string = "localhost:7000" // Edit this
