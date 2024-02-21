9166dd269e486468822fd3e36e0480657ed44caf
Ahri <ahri.public@gmail.com>
2024-02-21 17:43:13 +0900

    5.1  Request model inference including the information of the provider.

--- schedulerCommunicator/getAgentAddress.go
+++ schedulerCommunicator/getAgentAddress.go
@@ -3,6 +3,7 @@ package schedulerCommunicator
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -13,6 +14,45 @@ import (
 /* Scheduler Struct */
 type schedulerResponse struct {
 	Address string `json:"address"`
+}
+
+/* Obtain the address of the GPU node from the scheduler. */
+/* Information on provider, model, and version is required. */
+func GetAgentAddressV2(provider string, model string, version string) (string, error) {
+	log.Println("* (System) Model information: ▽▽▽▽▽▽▽▽▽▽")
+	log.Println("provider:", provider)
+	log.Println("Model:", model)
+	log.Println("Version:", version)
+
+	// Url setting
+	urlQuery := fmt.Sprintf("?id=%s&model=%s&version=%s", provider, model, version)
+	url := fmt.Sprintf("http://%s/request%s", setting.SchedulerUrl, urlQuery)
+	resp, err := http.Post(url, "application/json", nil)
+	if err != nil {
+		return "", err
+	}
+
+	// Start listening when the status code is 200
+	if resp.StatusCode == http.StatusOK {
+		log.Println("* (System) Received a status code 200 from the Scheduler.")
+
+		// Read body
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return "", err
+		}
+
+		// Json decode
+		var schedulerResponse schedulerResponse
+		if err := json.Unmarshal(body, &schedulerResponse); err != nil {
+			return "", err
+		}
+		log.Println("* (System) Get GPU-node address:", schedulerResponse.Address)
+
+		return schedulerResponse.Address, nil
+	} else {
+		return "", errors.New("scheduler error")
+	}
 }
 
 /* Obtain the address of the GPU node from the scheduler. */
@@ -59,6 +99,6 @@ func GetAgentAddress(model string, version string) (string, error) {
 
 		return schedulerResponse.Address, nil
 	} else {
-		return "", errors.New("Scheduler error")
+		return "", errors.New("scheduler error")
 	}
 }
